e73593c4c55545c84a229f8814091975387381f5
Alyssa Evans <alyda@me.com>
2024-05-21 18:37:12 +0000

    03.09 - questionable

--- exercises/03_ticket_v1/09_heap/src/lib.rs
+++ exercises/03_ticket_v1/09_heap/src/lib.rs
@@ -11,9 +11,11 @@ mod tests {
     use super::Ticket;
     use std::mem::size_of;
 
+    let my_mem = size_of::<usize>();
+
     #[test]
     fn string_size() {
-        assert_eq!(size_of::<String>(), todo!());
+        assert_eq!(size_of::<String>(), my_mem * 3); // 24 - pointer: usize, length: usize capacity: usize
     }
 
     #[test]
@@ -23,6 +25,6 @@ mod tests {
         // but, in general, the memory layout of structs is a more complex topic.
         // If you're curious, check out the "Data layout" section of the Rustonomicon
         // https://doc.rust-lang.org/nomicon/data.html for more information.
-        assert_eq!(size_of::<Ticket>(), todo!());
+        assert_eq!(size_of::<Ticket>(), my_mem * 3 * 3); // 72 I don't know why "72" is intuitive here. I guess it's because the struct has 3 strings...
     }
 }
