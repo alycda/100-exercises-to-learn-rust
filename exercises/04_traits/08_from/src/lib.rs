f20c5d23008a765838b541edd50a4d732342009d
Alyssa Evans <alyda@me.com>
2024-05-24 18:52:24 +0000

    04.08 - with help from code suggestions (auto implement)

--- exercises/04_traits/08_from/src/lib.rs
+++ exercises/04_traits/08_from/src/lib.rs
@@ -4,6 +4,19 @@ pub struct WrappingU32 {
     value: u32,
 }
 
+impl From<u32> for WrappingU32 {
+    fn from(value: u32) -> Self {
+        WrappingU32 {
+            value,
+        }
+    }
+//     fn from(self) -> WrappingU32 {
+//         WrappingU32 {
+//             value: self
+//         }
+//     }
+}
+
 fn example() {
     let wrapping: WrappingU32 = 42.into();
     let wrapping = WrappingU32::from(42);
