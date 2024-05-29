8b8b1bf36040b3e0744d163f2882cef22854e28e
Alyssa Evans <alyda@me.com>
2024-05-29 16:33:26 +0000

    05.13 - fixed with help from solutions

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -12,9 +12,19 @@ impl TryFrom<String> for Status {
     type Error = &'static str;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
-        match value.to_lowercase() {
-            String::from("todo") => Ok(Status::ToDo),
-            _ => Err("no match")
+        value.as_str().try_into()
+    }
+}
+
+impl TryFrom<&str> for Status {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err("ERROR")
         }
     }
 }
