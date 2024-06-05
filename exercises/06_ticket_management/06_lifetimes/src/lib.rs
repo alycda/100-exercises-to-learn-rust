0de9860a3544d8b6978790ac004c9608829410ff
Alyssa Evans <alyda@me.com>
2024-06-05 19:23:10 +0000

    06.06

--- exercises/06_ticket_management/06_lifetimes/src/lib.rs
+++ exercises/06_ticket_management/06_lifetimes/src/lib.rs
@@ -36,12 +36,12 @@ impl TicketStore {
     }
 }
 
-impl<'a> IntoIterator for &'a TicketStore {
+impl<'a> IntoIterator for &TicketStore {
     type Item<'a> = &'a Ticket;
     type IntoIter = std::vec::IntoIter<'a, Self::Item>;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.tickets.iter()
+        self.tickets.into_iter()
     }
 }
 
