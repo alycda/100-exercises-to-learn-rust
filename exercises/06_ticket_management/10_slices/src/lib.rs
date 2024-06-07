129435650e445748c1ae87ff0ece193d1b0e6889
Alyssa Evans <alyda@me.com>
2024-06-07 23:45:16 +0000

    06.10

--- exercises/06_ticket_management/10_slices/src/lib.rs
+++ exercises/06_ticket_management/10_slices/src/lib.rs
@@ -1,6 +1,10 @@
 // TODO: Define a function named `sum` that takes a reference to a slice of `u32` and returns the sum of all
 //  elements in the slice.
 
+pub fn sum(summands: &[u32]) -> u32 {
+    summands.iter().sum::<u32>()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
